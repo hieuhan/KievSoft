1fb034686d76629ad38e18581ac18e6e275452e9
hieuht <hantrunghieu@gmail.com>
2019-02-17 11:05:04 +0700

    ...

---
+++ DbHelpers/DbHelper.Core.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbHelpers
+{
+    public partial class DbHelper
+    {
+        #region Constructors
+
+        public DbHelper(DbProviderFactory providerFactory, string connectionString)
+        {
+            if (providerFactory == null)
+                throw new ArgumentNullException("providerFactory", "You must provide a DbProviderFactory instance.");
+
+            if (String.IsNullOrEmpty(connectionString))
+                throw new ArgumentException("The connection string cannot be empty.", "connectionString");
+
+            _factory = providerFactory;
+            _connectionString = connectionString;
+        }
+
+        public DbHelper(string connectionStringName)
+        {
+            ConnectionStringSettings css = ConfigurationManager.ConnectionStrings[connectionStringName];
+
+            if (css == null)
+                throw new ArgumentException("The connection string you specified does not exist in your configuration file.");
+
+            _factory = DbProviderFactories.GetFactory(css.ProviderName);
+            _connectionString = css.ConnectionString;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private DbProviderFactory _factory;
+        private string _connectionString;
+
+        public DbProviderFactory Factory
+        {
+            get
+            {
+                return _factory;
+            }
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return _connectionString;
+            }
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        protected static void FillFromReader(DbDataReader reader, int startRecord, int maxRecords, Action<DbDataReader> action)
+        {
+            if (startRecord < 0)
+                throw new ArgumentOutOfRangeException("startRecord", "StartRecord must be zero or higher.");
+
+            while (startRecord > 0)
+            {
+                if (!reader.Read())
+                    return;
+
+                startRecord--;
+            }
+
+            if (maxRecords > 0)
+            {
+                int i = 0;
+
+                while (i < maxRecords && reader.Read())
+                {
+                    action(reader);
+                    i++;
+                }
+            }
+            else
+            {
+                while (reader.Read())
+                    action(reader);
+            }
+        }
+
+        protected static async Task FillFromReaderAsync(DbDataReader reader, int startRecord, int maxRecords, Action<DbDataReader> action)
+        {
+            if (startRecord < 0)
+                throw new ArgumentOutOfRangeException("startRecord", "StartRecord must be zero or higher.");
+
+            while (startRecord > 0)
+            {
+                if (!await reader.ReadAsync())
+                    return;
+
+                startRecord--;
+            }
+
+            if (maxRecords > 0)
+            {
+                int i = 0;
+
+                while (i < maxRecords && await reader.ReadAsync())
+                {
+                    action(reader);
+                    i++;
+                }
+            }
+            else
+            {
+                while (await reader.ReadAsync())
+                    action(reader);
+            }
+        }
+
+        private string GetProviderParameterFormatString()
+        {
+            var builder = Factory.CreateCommandBuilder();
+            var type = builder.GetType();
+            var method = type.GetMethod("GetParameterPlaceholder", BindingFlags.NonPublic | BindingFlags.Instance);
+            var index = 42;
+            var parameterName = method.Invoke(builder, new object[] { index }).ToString();
+            return parameterName.Replace(index.ToString(CultureInfo.InvariantCulture), "{0}");
+        }
+
+        #endregion
+
+        #region Helper Methods and Extension Points
+
+        public DbConnection CreateConnection()
+        {
+            DbConnection connection = Factory.CreateConnection();
+            connection.ConnectionString = ConnectionString;
+
+            return connection;
+        }
+
+        public DbCommand CreateCommand(string commandText, params object[] parameters)
+        {
+            var len = parameters.Length;
+
+            var command = Factory.CreateCommand();
+            command.CommandType = CommandType.Text;
+
+            if (len > 0)
+            {
+                var formatValues = new string[len];
+
+                for (var i = 0; i < len; i++)
+                {
+                    var parameter = parameters[i];
+                    var rawValue = parameter as RawValue;
+
+                    if (rawValue != null)
+                    {
+                        formatValues[i] = rawValue.Value;
+                    }
+                    else
+                    {
+                        var dbParameter = Factory.CreateParameter();
+                        var name = CreateParameterName(i);
+
+                        dbParameter.ParameterName = name;
+                        dbParameter.Value = parameter ?? DBNull.Value;
+
+                        formatValues[i] = name;
+                        command.Parameters.Add(dbParameter);
+                    }
+                }
+
+                command.CommandText = String.Format(commandText, formatValues);
+            }
+            else
+            {
+                command.CommandText = commandText;
+            }
+
+            return command;
+        }
+
+        private string _parameterFormat;
+
+        protected virtual string CreateParameterName(int index)
+        {
+            if (_parameterFormat == null)
+                _parameterFormat = GetProviderParameterFormatString();
+
+            return String.Format(_parameterFormat, index);
+        }
+
+        protected virtual Converter<object, T> GetTypeConverter<T>()
+        {
+            return (object o) => (T)DbConvert.To<T>(o);
+        }
+
+        protected virtual Converter<DbDataReader, T> GetDataReaderConverter<T>()
+            where T : new()
+        {
+            return new DataReaderConverter<T>().Convert;
+        }
+
+        protected virtual void OnExecuteCommand(DbCommand command)
+        { }
+
+        #endregion
+    }
+}
