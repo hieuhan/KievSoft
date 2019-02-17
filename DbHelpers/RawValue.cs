d0baf6db4794d6d7d1ee461a0e53689610741635
hieuht <hantrunghieu@gmail.com>
2019-02-17 10:48:51 +0700

    ...

---
+++ DbHelpers/RawValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbHelpers
+{
+    public class RawValue
+    {
+        public RawValue(string value)
+        {
+            this.Value = value;
+        }
+
+        public string Value { get; private set; }
+    }
+}
